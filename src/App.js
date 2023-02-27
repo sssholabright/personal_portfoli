32e6a4f55ffed714ee3049970c42fb69a6dfcaf4
Mujeeb Adejobi <adejobimujeeb5@gmail.com>
2023-02-27 11:44:05 +0100

    Changes to be committed: 	deleted:    public/me.jpg 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import './App.css';
-/*import About from './Container/AboutMe/About';
-import Contact from './Container/ContactMe/Contact';
-import Experience from './Container/Experience/Experience';
-import Footer from './Container/Footer/Footer';
-import Header from './Container/Header/Header';
-import Nav from './Container/Navigation/Nav';
-import Portfolio from './Container/Resume/Portfolio';
-import Services from './Container/Services/Services';
-import Testimonial from './Container/Testimonials/Testimonial';*/
 import Form from './Ict_pay/Components/Form'
-import Pay from './Ict_pay/Components/Pay'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -25,19 +15,6 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-  
-    /*<>
-      <Header />
-      <Nav />
-
-      <About />
-      <Experience />
-      <Services />
-      <Portfolio />
-      <Testimonial />
-      <Contact />
-      <Footer />
-  </>*/
   <RouterProvider router={router} />
     
   );
